93b02a2cc2a11d94d46e472210c434d3b5e23fa0
lnguyen24794 <lamnt@fasttrack.edu.vn>
2019-01-14 20:10:00 +0700

    Revert "prism editor"
    This reverts commit b633a0fb12a0def7973b36ce27d8c72075fd9049.

--- src/views/home/contact.vue
+++ src/views/home/contact.vue
@@ -1,76 +1,15 @@
 <template>
-  <div id="app" class="min-h-screen">
-    <!-- <header class="header bg-white">
-      <div class="hero py-8 sm:pt-24 text-center">
-        
-        <div class="hero-options my-8 w-64 max-w-sm sm:w-full mx-auto">
-            <label for="ln" class="">
-              <input type="checkbox" name="ln" v-model="lineNumbers">
-              Line Numbers
-            </label>
-            <label for="ln" class="ml-4">
-              <input type="checkbox" name="ln" v-model="readonly">
-              Readonly
-            </label>
-        </div>
-        
-      </div>
-    </header> -->
-    <main class="main max-w-lg mx-auto my-0 p-0">
-      <Editor
-        class="my-editor"
-        language="html"
-        v-model="code"
-        :line-numbers="lineNumbers"
-        :readonly="readonly"
-        ></Editor>
-    </main>
-  </div>
+    <div>
+        contact
+    </div>
 </template>
 
 <script>
-/* global Prism */
-import "prismjs";
-import "@/assets/editorstyle.css";
-// import "prismjs/themes/prism-tomorrow.css";
-import Editor from "@/components/Editor.vue";
-
-export default {
-  name: "app",
-  components: {
-    Editor
-  },
-  data: () => ({
-    lineNumbers: true,
-    readonly: true,
-    /* eslint-disable */
-    code: `<template>
-  <div id="app">
-    <p>{{ message }}</p>
-    <input v-model="message">
-  </div>
-</template>
-<script>
-export default {
-  data:() => ({
-    message: 'Hello Vue!'
-  })
-}
-<\/script>
-<style>
-#app {
-  color: #2ecc71
-}
-</style>` /* eslint-enable */
-  }),
-  mounted() {
-    console.log(Prism);
-  }
-};
+    export default {
+        
+    }
 </script>
 
-<style>
-.my-editor {
-  max-height: 378px;
-}
+<style scoped>
+
 </style>
