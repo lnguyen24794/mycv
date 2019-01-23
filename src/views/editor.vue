269bbb9b62e12b117215745c981fd58fff3225a2
lnguyen24794 <lnguyen24794@gmail.com>
2019-01-24 00:05:46 +0700

    a

--- src/views/editor.vue
+++ src/views/editor.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-     <editor v-model="modal"/>
+     <editor v-model="modal" :options="options"/>
      <viewer v-model="modal"/>
   </div>
 </template>
 
 <script>
+import "tui-editor";
+import 'tui-editor/dist/tui-editor-extChart';
+
+import 'tui-editor/dist/tui-editor-extColorSyntax.js';
 import 'tui-editor/dist/tui-editor.css';
 import 'tui-editor/dist/tui-editor-contents.css';
+import  'tui-color-picker/dist/tui-color-picker.css';
+
 import 'codemirror/lib/codemirror.css';
+
 import Editor from '@toast-ui/vue-editor/src/Editor.vue'
 
 import 'highlight.js/styles/github.css';
@@ -21,7 +28,16 @@ export default {
   },
   data(){
     return {
-      modal:""
+      modal:"",
+      options: {
+    minHeight: '200px',
+    language: 'en_US',
+    useCommandShortcut: true,
+    useDefaultHTMLSanitizer: true,
+    usageStatistics: true,
+    hideModeSwitch: false,
+    exts: ['scrollSync', 'colorSyntax', 'uml', 'chart', 'mark', 'table'],
+}
     }
   }
 }
